dc22acbbdcfe339c0e76263510aeb924cd6b95b3
Vlad <vapr125@gmail.com>
2026-04-20 11:46:21 +0300

    Создана модель ProcessInfoModel

---
+++ HardwareMonitor/Models/ProcessInfoModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace HardwareMonitor.Models
+{
+    /// <summary>
+    /// Модель данных процесса с поддержкой INotifyPropertyChanged
+    /// </summary>
+    public class ProcessInfoModel : INotifyPropertyChanged
+    {
+        private string _name;
+        private string _status;
+        private double _cpuUsage;
+        private long _memoryBytes;
+
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+
+        public string Status
+        {
+            get { return _status; }
+            set { SetProperty(ref _status, value); }
+        }
+
+        public double CpuUsage
+        {
+            get { return _cpuUsage; }
+            set { SetProperty(ref _cpuUsage, value); }
+        }
+
+        public long MemoryBytes
+        {
+            get { return _memoryBytes; }
+            set { SetProperty(ref _memoryBytes, value); }
+        }
+
+        public double MemoryMB
+        {
+            get { return _memoryBytes / 1024.0 / 1024.0; }
+        }
+
+        public ProcessInfoModel()
+        {
+            _name = "Неизвестно";
+            _status = "Неизвестно";
+            _cpuUsage = 0;
+            _memoryBytes = 0;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return false;
+            }
+            storage = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+    }
+}
